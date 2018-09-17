c5aaf7913d7c8e9e736a3989b50971a8c26b37cd
PT1972 <tetsuya.ogawa87@gmail.com>
2018-09-17 13:56:29 +0900

    今日の日付部分に記しつける

--- app/javascript/src/javascripts/pc/components/company_ipo_calender/company_ipo_calender_body.vue
+++ app/javascript/src/javascripts/pc/components/company_ipo_calender/company_ipo_calender_body.vue
@@ -14,7 +14,9 @@
             </thead>
             <tbody>
                 <tr v-for="dateRow in dateRows">
-                    <td v-for="date in dateRow" class="c-companyIpoCalender_date" v-bind:class="[date.kind, date.bbPeriod, date.pPeriod, date.listingDate, date.winningDate]">{{date['date']}}</td>
+                    <td v-for="date in dateRow" class="c-companyIpoCalender_date" v-bind:class="[date.kind, date.bbPeriod, date.pPeriod, date.listingDate, date.winningDate, date.today]">
+                        <span>{{date['date']}}</span>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -42,13 +44,16 @@
               const option = p_start.getTime() == target.getTime() ? '--start' : target.getTime() == p_end.getTime() ? '--end' : ''
               return p_start <= target && target <= p_end ? `pPeriod${option}` : ''
           },
-          isListingdDate(year, month, date) {
+          isListingDate(year, month, date) {
               const listingDate = new Date(this.parsedIpoInformation['listing_date'] + 'T00:00')
               return listingDate.getTime() == new Date(year, month - 1, date).getTime()
           },
           isWinningDate(year, month, date) {
               const winningDate = new Date(this.parsedIpoInformation['winning_date'] + 'T00:00')
               return winningDate.getTime() == new Date(year, month - 1, date).getTime()
+          },
+          isToday(year, month, date) {
+              return new Date().setHours(0, 0, 0, 0) == new Date(year, month - 1, date).getTime()
           }
         },
         computed: {
@@ -60,16 +65,17 @@
             let calenderDate = [...Array(42).keys()].map(i => {
                 const bookBuildingPeriod = this.bookBuildingClass(this.year, this.month, i - firstDateWeekDay + 1)
                 const purchasePeriod = this.purchaseClass(this.year, this.month, i - firstDateWeekDay + 1)
-                const listingDate = this.isListingdDate(this.year, this.month, i - firstDateWeekDay + 1) ? 'isListingDate' : ''
+                const listingDate = this.isListingDate(this.year, this.month, i - firstDateWeekDay + 1) ? 'isListingDate' : ''
                 const winningDate = this.isWinningDate(this.year, this.month, i - firstDateWeekDay + 1) ? 'isWinningDate' : ''
+                const isToday = this.isToday(this.year, this.month, i - firstDateWeekDay + 1) ? 'today' : ''
                 if(i < firstDateWeekDay) {
-                    return { date: prevMonthLastDate + ++i - firstDateWeekDay, kind: 'prev', bbPeriod: bookBuildingPeriod, pPeriod: purchasePeriod, listingDate: listingDate, winningDate: winningDate }
+                    return { date: prevMonthLastDate + ++i - firstDateWeekDay, kind: 'prev', bbPeriod: bookBuildingPeriod, pPeriod: purchasePeriod, listingDate: listingDate, winningDate: winningDate, today: isToday }
                 } else if (i == firstDateWeekDay){
-                    return { date: ++i - firstDateWeekDay, bbPeriod: bookBuildingPeriod, pPeriod: purchasePeriod, listingDate: listingDate, winningDate: winningDate }
+                    return { date: ++i - firstDateWeekDay, bbPeriod: bookBuildingPeriod, pPeriod: purchasePeriod, listingDate: listingDate, winningDate: winningDate, today: isToday }
                 } else if(firstDateWeekDay < i && i < lastDate + firstDateWeekDay) {
-                    return { date: ++i - firstDateWeekDay, bbPeriod: bookBuildingPeriod, pPeriod: purchasePeriod, listingDate: listingDate, winningDate: winningDate }
+                    return { date: ++i - firstDateWeekDay, bbPeriod: bookBuildingPeriod, pPeriod: purchasePeriod, listingDate: listingDate, winningDate: winningDate, today: isToday }
                 } else {
-                    return { date: ++i - lastDate - firstDateWeekDay, kind: 'next', bbPeriod: bookBuildingPeriod, pPeriod: purchasePeriod, listingDate: listingDate, winningDate: winningDate }
+                    return { date: ++i - lastDate - firstDateWeekDay, kind: 'next', bbPeriod: bookBuildingPeriod, pPeriod: purchasePeriod, listingDate: listingDate, winningDate: winningDate, today: isToday }
                 }
             })
             for(let i = 0; i < Math.ceil(calenderDate.length / 7); i++) {
