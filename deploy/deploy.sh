7c5ed72505ab53c4b98a1fe0f37e6b145a4c4994
PT1972 <tetsuya.ogawa87@gmail.com>
2018-09-29 12:04:02 +0900

    deployスクリプト調整

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ ssh -t ipo-juku `cat <<EOF
 cd ipo-juku &&
 git pull origin master &&
 docker build -f docker/rails/Dockerfile . -t ipo-juku &&
-docker stop $(docker ps | grep -v CONTAINER  | cut -d ' ' -f1) &&
+docker stop \\$(docker ps | grep -v CONTAINER  | cut -d ' ' -f1) &&
 docker run -d -p 3000:3000 ipo-juku &&
-docker rmi -f $(docker images | grep none | sed -r -e 's/ +/ /g' | cut -d ' ' -f3)
+docker rmi -f \\$(docker images | grep none | sed -r -e 's/ +/ /g' | cut -d ' ' -f3)
 EOF`
