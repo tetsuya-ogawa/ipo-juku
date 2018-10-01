b07446738ffd9ce7e5ec98558dabbabd0ec0eeaf
PT1972 <tetsuya.ogawa87@gmail.com>
2018-10-01 11:59:11 +0900

    応急処置としてspはviewport設定なし

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :detect_device_variant
+  
   def detect_device_variant
     request.variant = request.user_agent&.match?(/iPod|iPhone|iPad|Android/) ? :sp : :pc
   end
