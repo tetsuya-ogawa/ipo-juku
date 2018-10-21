185cdb6305736dc8dfa41bdd945171f4a493fb8e
PT1972 <tetsuya.ogawa87@gmail.com>
2018-10-21 11:06:47 +0900

    ユーザー側の記事ルーティングは隠す

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'companies#index'
   resources :companies, only: [:index, :show]
-  resources :articles, only: [:index, :show]
+  # resources :articles, only: [:index, :show]
 
   namespace :admins, as: :admin, path: :admin do
     root 'top#index'
