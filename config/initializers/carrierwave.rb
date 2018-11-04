081942ea4c2b92e713e7be9e2ee7440a0cbac2c9
PT1972 <tetsuya.ogawa87@gmail.com>
2018-11-04 10:42:34 +0900

    AWSのキーは環境変数見るように

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: '',
-      aws_secret_access_key: '+GczB09v',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1'
   }
 
